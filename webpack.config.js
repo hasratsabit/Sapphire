845f0047f243ffdb291e7c6967324a2f9a1c4efd
Hasrat <hsabit111@gmail.com>
2018-03-21 13:14:29 -0400

    Webpack is modefied

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const extractPlugin = new ExtractTextPlugin({
     filename: 'style-[contenthash:10].css'
@@ -71,6 +72,9 @@ module.exports = {
         extractPlugin,
         new HtmlWebpackPlugin({
             template: 'src/index.html'
-        })
+        }),
+        // new CopyWebpackPlugin({
+            
+        // })
     ]
 }
