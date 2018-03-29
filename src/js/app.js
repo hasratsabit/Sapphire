e277f9c06872bdb9a074825a164d96dd7c89573d
Hasrat <hsabit111@gmail.com>
2018-03-29 10:22:21 -0400

    The sticky nav is finnished

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,45 @@ import '../icons/css/fa-svg-with-js.css';
 import '../icons/js/fontawesome-all.js';
 
 
+const stickyNav = (() => {
+
+    function debounce(func, wait = 10, immediate = true) {
+        var timeout;
+        return function() {
+          var context = this, args = arguments;
+          var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+          };
+          var callNow = immediate && !timeout;
+          clearTimeout(timeout);
+          timeout = setTimeout(later, wait);
+          if (callNow) func.apply(context, args);
+        };
+      };
+
+    const background = document.querySelector('#background');
+    const logo = document.querySelector('#logo');
+    const nav = document.querySelector('#nav');
+
+    function fixedNav() {
+        if(window.scrollY >= 100) {
+            background.classList.add('background-active');
+            logo.classList.add('logo-active');
+            nav.classList.add('nav-expanded');
+        }else {
+            background.classList.remove('background-active');
+            logo.classList.remove('logo-active');
+            nav.classList.remove('nav-expanded');
+        }
+    }
+
+    window.addEventListener('scroll', debounce(fixedNav))
+})()
+
+
+
+
 
 
 
