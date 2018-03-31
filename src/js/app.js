5d958fe6befa470d8514624c8a2c0b22f8ded18c
Hasrat <hsabit111@gmail.com>
2018-03-31 10:24:16 -0400

    Navigation fixed

--- src/js/app.js
+++ src/js/app.js
@@ -2,42 +2,54 @@ import '../css/style.scss';
 import '../icons/css/fa-svg-with-js.css';
 import '../icons/js/fontawesome-all.js';
 
+import './Modules/stickyNav';
+import './Modules/smoothScroll';
 
-const stickyNav = (() => {
 
-    function debounce(func, wait = 10, immediate = true) {
-        var timeout;
-        return function() {
-          var context = this, args = arguments;
-          var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-          };
-          var callNow = immediate && !timeout;
-          clearTimeout(timeout);
-          timeout = setTimeout(later, wait);
-          if (callNow) func.apply(context, args);
-        };
-      };
 
-    const background = document.querySelector('#background');
-    const logo = document.querySelector('#logo');
-    const nav = document.querySelector('#nav');
+// const revealOnScroll = (() => {
 
-    function fixedNav() {
-        if(window.scrollY >= 100) {
-            background.classList.add('background-active');
-            logo.classList.add('logo-active');
-            nav.classList.add('nav-expanded');
-        }else {
-            background.classList.remove('background-active');
-            logo.classList.remove('logo-active');
-            nav.classList.remove('nav-expanded');
-        }
-    }
+//     const slideElems = document.querySelectorAll('.slideUp');
 
-    window.addEventListener('scroll', debounce(fixedNav))
-})()
+//     function revealElem() {
+//         slideElems.forEach((elem, i) => {
+        
+//             const slideInAt = (window.scrollY + window.innerHeight) -  elem.clientHeight / 2;
+//             const isHalfShown = slideInAt > elem.offsetTop;
+//             if (isHalfShown) {
+//                 setTimeout(() => {
+//                     elem.classList.add('slideUp--active'); 
+//                 }, 400 * (i + 1))
+//               }
+    
+//         })
+//     }
+
+//     window.addEventListener('scroll', revealElem);
+
+// })()
+
+
+// window.addEventListener('scroll', () => {
+
+//     const elements = document.querySelectorAll('.slideUp');
+
+//     elements.forEach((elem, i) => {
+
+//         const slideInAt = (window.scrollY + window.innerHeight) - elem.offsetTop;
+//         const isHalfShown = slideInAt >= elem.offsetTop;
+
+//         if(isHalfShown) {
+//             setTimeout(() => {
+//                 elem.classList.add('slideUp--active');
+//             }, 300 * (i + 1));
+//         }
+//     })
+// })
+
+
+
+
 
 
 
